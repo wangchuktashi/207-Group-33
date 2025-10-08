f2fd35b00f2525fc2cc3f6fce2aacea800dcb8ba
Jonathan Hii <n8769303@qut.edu.au>
2025-10-08 15:27:46 +1000

    added relationships to model

--- a2/models.py
+++ a2/models.py
@@ -11,8 +11,7 @@ class User(db.Model, UserMixin):
     mobile_number = db.Column(db.String(10), unique=True, nullable=False)
     email_id = db.Column(db.String(100), index=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
-    # relation to call user.comments and comment.created_by
-    comments = db.relationship('Comment', backref='user')
+    comments = db.relationship('Comment', backref='user')               # relation to call user.comments and comment.created_by
     
     # string print method
     def __repr__(self):
@@ -22,8 +21,8 @@ class User(db.Model, UserMixin):
 class Event(db.Model):
     __tablename__ = "events"
     id = db.Column(db.Integer, primary_key=True)
-    # user id (FK)
-    venue_id = db.Column(db.Integer, db.ForeignKey('venues.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))          # relationship to event
+    venue_id = db.Column(db.Integer, db.ForeignKey('venues.id'))        # relationship to venue
     sports_type = db.Column(db.String(64), unique=True, index=True)
     event_title = db.Column(db.String(256))
     home_team_name = db.Column(db.String(64))
@@ -31,7 +30,7 @@ class Event(db.Model):
     event_image = db.Column(db.String(64))
     start_datetime = db.Column(db.DateTime, default=datetime.now())
     end_datetime = db.Column(db.DateTime, default=datetime.now())
-    # relationship to user
+    comments = db.relationship('Comment', backref='event')              # relation to call event.comments and comment.event
 
     # string print method
     def __repr__(self):
@@ -41,10 +40,9 @@ class Event(db.Model):
 class EventStatus(db.Model):
     __tablename__ = "eventStatus"
     id = db.Column(db.Integer, primary_key=True)
-    event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
+    event_id = db.Column(db.Integer, db.ForeignKey('events.id'))        # relationship to event
     event_status = db.Column(db.String(32), index=True)
     event_status_date = db.Column(db.DateTime, default=datetime.now())
-    # relationship to event
 
     # string print method
     def __repr__(self):
@@ -57,7 +55,6 @@ class Venue(db.Model):
     venue_name = db.Column(db.String(128))
     venue_address = db.Column(db.String(256), nullable=True)
     capacity = db.Column(db.Integer)
-    # relationship to event
 
     # string print method
     def __repr__(self):
@@ -67,11 +64,10 @@ class Venue(db.Model):
 class Comment(db.Model):
     __tablename__ = "comments"
     id = db.Column(db.Integer, primary_key=True)
-    # user id (FK)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))          # relationship to user
     event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
     text = db.Column(db.String(400))
-    posted_date = db.Column(db.DateTime, default=datetime.now())
-    # relationship to user
+    posted_date = db.Column(db.DateTime, default=datetime.now()) 
 
     # string print method
     def __repr__(self):
@@ -81,12 +77,10 @@ class Comment(db.Model):
 class Booking(db.Model):
     __tablename__ = "bookings"
     id = db.Column(db.Integer, primary_key=True)
-    # user id (FK)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))          # relationship to user
     event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
     booking_date = db.Column(db.DateTime, default=datetime.now())
     booking_quantity = db.Column(db.Integer)
-    # relationship to user
-    # relationship to event
 
     # string print method
     def __repr__(self):
@@ -96,11 +90,10 @@ class Booking(db.Model):
 class Ticket(db.Model):
     __tablename__ = "tickets"
     id = db.Column(db.Integer, primary_key=True)
-    # booking id (FK)
-    event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
-    ticket_price = db.Column(db.Float)
+    booking_id = db.Column(db.Integer, db.ForeignKey('bookings.id'))    # relationship to booking
+    event_id = db.Column(db.Integer, db.ForeignKey('events.id'))        # relationship to event
+    ticket_price = db.Column(db.Float, nullable=False)  
     tickets_available = db.Column(db.Integer)
-    # relationship to booking
 
     # string print method
     def __repr__(self):
