4f7265e556892f93279997449fae40ca2449ac19
Jonathan Hii <n8769303@qut.edu.au>
2025-10-18 14:47:37 +1000

    mandy's auth.py

--- a2/website/auth.py
+++ a2/website/auth.py
@@ -8,6 +8,32 @@ from . import db
 # Create a blueprint - make sure all BPs have unique names
 auth_bp = Blueprint('auth', __name__)
 
+@auth_bp.route('/register', methods=['GET', 'POST'])
+def register():
+    register = RegisterForm()
+    #the validation of form is fine, HTTP request is POST
+    if (register.validate_on_submit()==True):
+            #get username, password and email from the form
+            uname = register.user_name.data
+            pwd = register.password.data
+            email = register.email_id.data
+            #check if a user exists
+            user = db.session.scalar(db.select(User).where(User.name==uname))
+            if user:#this returns true when user is not None
+                flash('Username already exists, please try another')
+                return redirect(url_for('auth.register'))
+            # don't store the password in plaintext!
+            pwd_hash = generate_password_hash(pwd)
+            #create a new User model object
+            new_user = User(name=uname, password_hash=pwd_hash, emailid=email)
+            db.session.add(new_user)
+            db.session.commit()
+            #commit to the database and redirect to HTML page
+            return redirect(url_for('index'))
+    #the else is called when the HTTP request calling this page is a GET
+    else:
+        return render_template('user.html', form=register, heading='Register')
+
 # this is a hint for a login function
 @auth_bp.route('/login', methods=['GET', 'POST'])
 # view function
@@ -32,3 +58,10 @@ def login():
         else:
             flash(error)
     return render_template('user.html', form=login_form, heading='Login')
+
+@auth_bp.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("You have been logged out and sent to the home page.")
+    return redirect(url_for('index'))
