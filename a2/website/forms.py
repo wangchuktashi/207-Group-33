6b6b51518450c22df04ff654e39f23afc76daf3d
robertt-13 <robertsunil9@gmail.com>
2025-10-18 16:47:18 +1000

    3

--- a2/website/forms.py
+++ a2/website/forms.py
@@ -17,13 +17,13 @@ class RegisterForm(FlaskForm):
     first_name = StringField("First name", validators=[InputRequired(), Length(max=100)])
     surname = StringField("Surname", validators=[InputRequired(), Length(max=100)])
     email_id = StringField("Email", validators=[InputRequired(), Email(), Length(max=100)])
+
+    # No regex: just require presence + sensible length range
     mobile_number = StringField(
         "Contact number",
-        validators=[
-            InputRequired(),
-            Regexp(r'^\+?\d[\d\s-]{6,}$', message="Enter a valid phone number")
-        ]
+        validators=[InputRequired(), Length(min=8, max=15, message="Enter 8–15 characters")]
     )
+
     street_address = StringField("Street address", validators=[InputRequired(), Length(max=255)])
 
     password = PasswordField("Password", validators=[
@@ -31,11 +31,9 @@ class RegisterForm(FlaskForm):
         Length(min=6),
         EqualTo('confirm', message="Passwords should match")
     ])
-    confirm = PasswordField("Confirm Password")
+    confirm  = PasswordField("Confirm Password")
 
-    # submit button
     submit = SubmitField("Register")
-
 # event form
 class EventForm(FlaskForm):
     name = StringField('Event Name', validators=[InputRequired()])
