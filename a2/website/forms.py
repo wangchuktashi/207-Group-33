1fc787ec417cd6d8962b808c652751b2ef74013b
Jonathan Hii <n8769303@qut.edu.au>
2025-10-18 16:23:58 +1000

    forms.py added event & comment form

--- a2/website/forms.py
+++ a2/website/forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField
 from wtforms.validators import InputRequired, Length, Email, EqualTo
+from flask_wtf.file import FileRequired, FileField, FileAllowed
+
+ALLOWED_FILE = {'PNG', 'JPG', 'JPEG', 'png', 'jpg', 'jpeg'}
 
 # creates the login information
 class LoginForm(FlaskForm):
@@ -19,3 +22,21 @@ class RegisterForm(FlaskForm):
 
     # submit button
     submit = SubmitField("Register")
+
+# event form
+class EventForm(FlaskForm):
+    name = StringField('Event Name', validators=[InputRequired()])
+    description = TextAreaField('Description', validators=[InputRequired()])
+    image = FileField('Destination Image', validators=[
+        FileRequired(message = 'Image cannot be empty'),
+        FileAllowed(ALLOWED_FILE, message='Only supports png, jpg, JPG, PNG')])
+    currency = StringField('Currency', validators=[InputRequired()])
+    submit = SubmitField("Create")
+    # more to add...
+
+
+# user comment form
+class CommentForm(FlaskForm):
+    text = TextAreaField('Comment', [InputRequired()])
+    submit = SubmitField('Create')
+    
