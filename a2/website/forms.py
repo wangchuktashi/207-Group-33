03f49a352898c7e7f5c11a1d5535e8b9c72b0d1a
robertt-13 <robertsunil9@gmail.com>
2025-10-18 16:24:37 +1000

    2

--- a2/website/forms.py
+++ a2/website/forms.py
@@ -1,11 +1,11 @@
 # website/forms.py
 from flask_wtf import FlaskForm
-from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField
-from wtforms.validators import InputRequired, Length, Email, EqualTo
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, Email, EqualTo, Regexp
 
 # Login form
 class LoginForm(FlaskForm):
-    email_id = StringField("Email", validators=[InputRequired(), Email()])
+    email_id = StringField("Email", validators=[InputRequired(), Email(), Length(max=100)])
     password = PasswordField("Password", validators=[InputRequired()])
     submit = SubmitField("Login")
 
@@ -14,18 +14,20 @@ class RegisterForm(FlaskForm):
     first_name = StringField("First name", validators=[InputRequired(), Length(max=100)])
     surname = StringField("Surname", validators=[InputRequired(), Length(max=100)])
     email_id = StringField("Email", validators=[InputRequired(), Email(), Length(max=100)])
-    mobile_number  = StringField(
+    mobile_number = StringField(
         "Contact number",
-        validators=[InputRequired(), Regexp(r'^\+?\d[\d\s-]{6,}$', message="Enter a valid phone number")]
+        validators=[
+            InputRequired(),
+            Regexp(r'^\+?\d[\d\s-]{6,}$', message="Enter a valid phone number")
+        ]
     )
-    street_address = StringField("Street address", validators=[InputRequired(), Length(max=256)])
+    street_address = StringField("Street address", validators=[InputRequired(), Length(max=255)])
 
     password = PasswordField("Password", validators=[
         InputRequired(),
         Length(min=6),
         EqualTo('confirm', message="Passwords should match")
     ])
-    confirm  = PasswordField("Confirm Password")
-    street_address = StringField("Street address", validators=[InputRequired(), Length(max=255)])
+    confirm = PasswordField("Confirm Password")
 
     submit = SubmitField("Register")
