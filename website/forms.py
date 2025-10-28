31725299076e21c2d0a47b24f4b2dc07a213febb
chmmcc <162306178+chmmcc@users.noreply.github.com>
2025-10-28 17:23:41 +1000

    Update forms.py

--- website/forms.py
+++ website/forms.py
@@ -1,32 +1,81 @@
+# website/forms.py
 from flask_wtf import FlaskForm
-from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField
-from wtforms.validators import InputRequired, Length, Email, EqualTo
+from wtforms.fields import TextAreaField, IntegerField,  SubmitField, StringField, PasswordField, SelectField, DateTimeLocalField
+from wtforms.validators import InputRequired, Length, Email, EqualTo, AnyOf, NumberRange
+from flask_wtf.file import FileRequired, FileField, FileAllowed
+from datetime import datetime
 
-# creates the login information
+ALLOWED_FILE = {'PNG', 'JPG', 'JPEG', 'png', 'jpg', 'jpeg'}
+
+# Login form
 class LoginForm(FlaskForm):
-    user_name=StringField("User Name", validators=[InputRequired('Enter user name')])
-    password=PasswordField("Password", validators=[InputRequired('Enter user password')])
+    email_id = StringField("Email", validators=[InputRequired(), Email(), Length(max=100)])
+    password = PasswordField("Password", validators=[InputRequired()])
     submit = SubmitField("Login")
 
- # this is the registration form
+# Registration form
 class RegisterForm(FlaskForm):
-    user_name=StringField("User Name", validators=[InputRequired()])
-    email = StringField("Email Address", validators=[Email("Please enter a valid email")])
-    # linking two fields - password should be equal to data entered in confirm
-    password=PasswordField("Password", validators=[InputRequired(),
-                  EqualTo('confirm', message="Passwords should match")])
-    confirm = PasswordField("Confirm Password")
+    first_name = StringField("First name", validators=[InputRequired(), Length(max=100)])
+    surname = StringField("Surname", validators=[InputRequired(), Length(max=100)])
+    email_id = StringField("Email", validators=[InputRequired(), Email(), Length(max=100)])
 
-    # submit button
+    # No regex: just require presence + sensible length range
+    mobile_number = StringField(
+        "Contact number",
+        validators=[InputRequired(), Length(min=8, max=15, message="Enter 8–15 characters")]
+    )
+
+    street_address = StringField("Street address", validators=[InputRequired(), Length(max=255)])
+
+    password = PasswordField("Password", validators=[
+        InputRequired(),
+        Length(min=6),
+        EqualTo('confirm', message="Passwords should match")
+    ])
+    confirm  = PasswordField("Confirm Password")
+
     submit = SubmitField("Register")
-
-# this is the booking form
-class BookingForm(FlaskForm):
-    name = StringField('Name')
-    submit = SubmitField('Book')
-
-# this is the event form
+# event form
 class EventForm(FlaskForm):
-    title = StringField('Title')
-    date = StringField('Date')
-    submit = SubmitField('Save')
+    event_title = StringField('Event Title', validators=[InputRequired(), Length(max=256)])
+    sport_type = SelectField(
+        'Sport Type',
+        choices=[
+            ('football', 'Football'),
+            ('basketball', 'Basketball'),
+            ('rugby', 'Rugby'),
+            ('cricket', 'Cricket'),
+            ('tennis', 'Tennis'),
+            ('hockey', 'Hockey'),
+        ],
+        validators=[
+            InputRequired(message='Please choose a sport'),
+            AnyOf(values=['football','basketball','rugby','cricket','tennis','hockey'],
+                  message='Invalid sport selected')
+        ]
+    )
+    home_team = StringField('Home Team', validators=[InputRequired(),  Length(max=64)])
+    away_team = StringField('Away Team', validators=[InputRequired(),  Length(max=64)])
+    start_datetime = DateTimeLocalField('Event Date', format='%Y-%m-%dT%H:%M', 
+                               validators=[InputRequired(message='Please choose a start date and time')])
+    end_datetime = DateTimeLocalField('Event Date', format='%Y-%m-%dT%H:%M',
+                           validators=[InputRequired(message='Please choose an end date time.')])
+    venue = StringField('Venue', validators=[InputRequired(), Length(max=128)])
+    total_tickets = IntegerField(
+        'Total Tickets',
+        validators=[InputRequired(), NumberRange(min=1, max=50000)],
+        render_kw={'min': 1, 'max': 50000, 'step': 1}
+    )
+    image = FileField('Event Image', validators=[
+        FileRequired(message = 'Image cannot be empty'),
+        FileAllowed(ALLOWED_FILE, message='Only supports png, jpg, JPG, PNG')])
+    description = TextAreaField('Description', validators=[InputRequired(message='Short description for attendees...')])
+    submit = SubmitField("Save Changes")
+    reset = SubmitField("Reset")
+
+
+# user comment form
+class CommentForm(FlaskForm):
+    text = TextAreaField('Comment', [InputRequired()])
+    submit = SubmitField('Create')
+    
