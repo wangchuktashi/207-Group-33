935b9a75f1f3c9fa8275289073b968ec0f6b05fc
Tashi Wangchuk <wangchukt2003@gmail.com>
2025-10-28 14:04:02 +1000

    Update __init__.py

--- a2/SportsZone/__init__.py
+++ a2/SportsZone/__init__.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
+# [A2-ADD] Flask-Login minimal setup
 from flask_login import LoginManager
 
 db = SQLAlchemy()
+
+login_manager = LoginManager()
+login_manager.login_view = 'main.login'
 
 def create_app():
     app = Flask(__name__, template_folder="templates", static_folder="static")
@@ -15,35 +19,14 @@ def create_app():
     db.init_app(app)
     Bootstrap5(app)
 
-# --- Flask-Login ---
-    login_manager = LoginManager()
-    login_manager.login_view = "auth.login"   # redirect here if not logged in
     login_manager.init_app(app)
 
-    from .models import User
-    @login_manager.user_loader
-    def load_user(user_id: str):
-        return db.session.scalar(db.select(User).where(User.id == int(user_id)))
-
-    # --- Blueprints ---
     from . import views
     app.register_blueprint(views.mainbp)
-
-    from . import auth
-    app.register_blueprint(auth.auth_bp)
-
-
-    # --- Error Handling ---
-    @app.errorhandler(404)
-    def not_found_error(error):
-        return render_template('404.html'), 404
-
-    @app.errorhandler(500)
-    def internal_error(error):
-        try:
-            db.session.rollback()
-        except Exception:
-            pass
-        return render_template('500.html'), 500
-    
     return app
+
+
+from .models import User
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
